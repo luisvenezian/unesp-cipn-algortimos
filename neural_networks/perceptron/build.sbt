b8c96e8b47c07af81d4eb352f57db54ed385c706
Luis Felipe A. Venezian <36893796+luisvenezian@users.noreply.github.com>
2024-05-02 08:35:29 -0300

    uhu!

--- neural_networks/perceptron/build.sbt
+++ neural_networks/perceptron/build.sbt
@@ -1,3 +1,5 @@
+import scala.collection.immutable.Seq
+
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.3"
@@ -7,3 +9,8 @@ lazy val root = (project in file("."))
     name := "perceptron",
     idePackagePrefix := Some("cipn")
   )
+
+libraryDependencies ++= Seq(
+  "com.lihaoyi" %% "ujson" % "1.4.2",
+  "com.lihaoyi" %% "os-lib" % "0.7.8"
+)
--- neural_networks/perceptron/src/main/scala/Perceptron.scala
+++ neural_networks/perceptron/src/main/scala/Perceptron.scala
@@ -1,5 +1,7 @@
 package cipn
 import scala.util.Random
+import ujson._
+import os._
 
 trait activationFunction {
   def limiar(x: Double): Double = if (x > 0) 1 else 0
@@ -14,7 +16,7 @@ trait activationFunction {
 
 class MultiOutputPerceptron (
                               val dataset: Dataset,
-                              val MAX_ITERATIONS: Int = 1000,
+                              val MAX_ITERATIONS: Int = 100,
                               val LEARNING_RATE: Double = 0.1
                             ) extends activationFunction
 {
@@ -24,6 +26,7 @@ class MultiOutputPerceptron (
   val input_size = features.head.size
   val output_size = expected_output.head.size
   var weights = Array.fill(output_size, input_size)(random.nextDouble)
+  var logs = List[Obj]()
   enum DatasetKind:
     case Training, Validation, Test
 
@@ -38,7 +41,26 @@ class MultiOutputPerceptron (
         val gradient = delta.map(d => x.map(_ * d))
         weights = weights.zip(gradient).map { case (a, b) => a.zip(b).map { case (c, d) => c + LEARNING_RATE * d } }
       }
+
+      logs = ujson.Obj.from(
+        Map(
+          "iteration" -> i,
+          "mse_trainig" -> mse(DatasetKind.Training),
+          "mse_validation" -> mse(DatasetKind.Validation),
+          "mse_test" -> mse(DatasetKind.Test),
+          "accuracy_training" -> validate(DatasetKind.Training),
+          "accuracy_validation" -> validate(DatasetKind.Validation),
+          "accuracy_test" -> validate(DatasetKind.Test),
+          "weights" -> weights.toList.map(_.toList).toList
+        )
+      ) :: logs
     }
+
+    // Save logs JSON file
+    val ts: String = System.currentTimeMillis().toString
+    val logsPath = os.pwd / "logs" / s"output-$ts.json"
+    os.write.over(logsPath, ujson.write(logs))
+
   }
 
   def validate(datasetKind: DatasetKind): Double = {
@@ -60,5 +82,21 @@ class MultiOutputPerceptron (
   def predict(x: List[Double]): List[Double] = {
     val z = weights.map(w => w.zip(x).map { case (a, b) => a * b }.sum)
     softmax(z).toList.map(_.round)
+  }
+
+  def mse(datasetKind: DatasetKind): Double = {
+    val features = datasetKind match {
+      case DatasetKind.Training => dataset.train.features
+      case DatasetKind.Validation => dataset.validation.features
+      case DatasetKind.Test => dataset.test.features
+    }
+    val expected_output = datasetKind match {
+      case DatasetKind.Training => dataset.train.encodedLabels
+      case DatasetKind.Validation => dataset.validation.encodedLabels
+      case DatasetKind.Test => dataset.test.encodedLabels
+    }
+    val predictions = features.map(predict)
+    val mse = expected_output.zip(predictions).map { case (a, b) => a.zip(b).map { case (c, d) => math.pow(c - d, 2) }.sum }.sum / features.size
+    mse
   }
 }
